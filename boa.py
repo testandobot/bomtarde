91a2de825819f80c6a96790a6389fb9de85f18a7
testandobot <68176907+testandobot@users.noreply.github.com>
2020-07-18 13:40:34 -0300

    Update boa.py

--- boa.py
+++ boa.py
@@ -37,7 +37,7 @@ def tweet_check(twitter_checked=True):
     if  horas == 23 and minutos == 30:
         Mensagem("Boa noite")
         time.sleep(60)
-    if horas == 13 and minutos == 45:
+    if horas == 16 and minutos == 45:
         print("tá")    
         
 if __name__ == '__main__':
